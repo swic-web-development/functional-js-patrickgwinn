199b68e73322c3b526b87834b4aae68990989e22
Patrick Gwinn <patrick.gwinn@student.swic.edu>
2025-03-03 19:37:26 -0600

    Add example of a callback function that manipulates a given number

--- src/index.js
+++ src/index.js
@@ -12,3 +12,20 @@ function calculateOrderCost(order) {
     return order.quantity * order.priceEa;
 }
 const calculateOrderCost = order => order.quantity * order.priceEa;
+
+function doSomethingWithNumber(number, callbackFunction) {
+    return callbackFunction(number);
+}
+funtion divideByTwo(num) {
+    return num / 2;
+}
+function multiplyByTwo(num) {
+    return num * 2;
+}
+function triple(num) {
+    return num * 3;
+}
+
+doSomethingWithNumber(10, divideByTwo);
+doSomethingWithNumber(10, multiplyByTwo);
+doSomethingWithNumber(10, triple);

f06cf9ced11182d1f05f36229a62e06ec84abdb2
Patrick Gwinn <patrick.gwinn@student.swic.edu>
2025-03-11 17:51:17 -0500

    Add filter method expamples to find the player with more frags than deaths for real this time

---
+++ src/arrays-practice.js
@@ -0,0 +1,19 @@
+const players = [
+    { name: 'DoomGuy', frags: 25, deaths: 0 },
+    { name: ' Slayer', frags: 25, deaths: 1 },
+    { name: 'Marine', frags: 12, deaths: 15 },
+    { name: 'Noob', frags: 0, deaths: 25 },
+]
+
+const playersWithMoreFragsThanDeaths = players.filter((player) => {
+   
+   console.log(`We are now looking at this player: ${player.name}`)
+    return player.frags > player.deaths
+})
+
+console.log(playersWithMoreFragsThanDeaths)
+
+const playersWithMoreDeathsThanFrags = players.filter((player) => player.frags > player.deaths)
+
+const totalFrags = players.reduce((total, player) => total + player.frags, 0)
+console.log(totalFrags)
